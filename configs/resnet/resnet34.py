9e353906ee3abaafbacd903fafc00845606f0e05
AokiMasataka <4okimasataka@gmail.com>
2022-12-26 22:19:55 +0900

    deep_stem=True and add eps arg

--- configs/resnet/resnet34.py
+++ configs/resnet/resnet34.py
@@ -2,8 +2,8 @@ backbone=dict(
     type='ResNet',
     block_type='BasicBlock',
     layers=(2, 2, 2, 2),
+    channels=(64, 128, 256, 512),
     in_channels=3,
     stem_width=64,
-    channels=(64, 128, 256, 512),
     act_config=dict(type='ReLU', inplace=True)
 )
