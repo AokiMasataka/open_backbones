05be5590f7ed287cde40b0b150f3d0ab6fca890a
AokiMasataka <4okimasataka@gmail.com>
2023-07-16 09:40:23 +0900

    impliment: norm_fn

--- openbacks/utils/base_module.py
+++ openbacks/utils/base_module.py
@@ -5,13 +5,25 @@ from torch import nn
 
 
 class BaseModule(nn.Module):
-    def __init__(self, init_config: dict = None, test_config: dict = None, norm_config: dict = None):
+    def __init__(self, init_config: dict = None, test_config: dict = None, norm_config: dict = None) -> None:
         super(BaseModule, self).__init__()
         self.init_config = init_config
         self.test_config = test_config
         self.norm_config = norm_config
 
-    def _init(self, prefix: str, checkpoint_filter_fn=None):
+        if norm_config is not None:
+            assert self.norm_config['mean'].__len__() == self.norm_config['std'].__len__()
+            ch = self.norm_config['mean'].__len__()
+            mean = torch.tensor(self.norm_config['mean'], dtype=torch.float).view(1, ch, 1, 1)
+            std = torch.tensor(self.norm_config['std'], dtype=torch.float).view(1, ch, 1, 1)
+            self.register_buffer('mean', mean)
+            self.register_buffer('std', std)
+            self.div = norm_config.get('div', None)
+            self.norm_fn = self._norm
+        else:
+            self.norm_fn = self._identity
+    
+    def init(self, prefix: str = None, checkpoint_filter_fn=None) -> None:
         if self.init_config is not None and self.init_config.get('pretrained', False):
             pretrained = self.init_config['pretrained']
             self._init_weight(
@@ -51,9 +63,15 @@ class BaseModule(nn.Module):
             print(f'[{prefix}] miss match keys:')
             for miss_match_key in miss_match_keys:
                 print('    ', miss_match_key)
-            
-            print(f'[{prefix}] number of match keys: {match_keys.__len__()}')
-            print(f'[{prefix}] number of miss match keys: {miss_match_keys.__len__()}')
 
         logging.info(msg=f'[{prefix}] number of match keys: {match_keys.__len__()}')
         logging.info(msg=f'[{prefix}] number of miss match keys: {miss_match_keys.__len__()}')
+
+    def _norm(self, images: torch.Tensor) -> torch.Tensor:
+        if self.div is None:
+            return (images - self.mean) / self.std
+        else:
+            return (images / self.div - self.mean) / self.std
+    
+    def _identity(self, images: torch.Tensor) -> torch.Tensor:
+        return images
